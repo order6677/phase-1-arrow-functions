d0b92ce486acd60b1adb0f5efdb738411338a15b
awadh <awadh.hassan@student.moringaschool.com>
2022-08-17 14:55:07 +0300

    assignment done

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = (num1,num2) => { return num1/num2};
+divide(2000/100);
+
+ 
+  const square = (y) => { return y * y };
+    
+  const add = (b,u) => {return b + u};
+  x = add(5,2)
+  console.log(x);
+
+  
+  
+
+
+
